fc269b4c4c2ec43f3c81c7f423fd987690778d2d
codeV124 <codex199201@gmail.com>
2025-03-15 18:17:31 +0200

    init

---
+++ src/app/about/page.tsx
@@ -0,0 +1,27 @@
+
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page | Racx Software Development LLC",
+  description: "This is About Page for Racx",
+  // other metadata
+};
+
+const AboutPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="About Page"
+        description="Whether you're looking to build a stunning website, automate your processes, or dive into the world of cryptocurrency trading, we have the expertise to help you succeed."
+      />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+    </>
+  );
+};
+
+export default AboutPage;
